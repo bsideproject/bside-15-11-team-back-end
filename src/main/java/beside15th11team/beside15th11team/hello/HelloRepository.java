759dcfc032065a1602f7ab40a9a1bd450e6289f3
isanghaessi <jesse1231@naver.com>
2023-05-20 19:51:12 +0900

    [oth] 누락된 @Repository 추가

--- src/main/java/beside15th11team/beside15th11team/hello/HelloRepository.java
+++ src/main/java/beside15th11team/beside15th11team/hello/HelloRepository.java
@@ -2,7 +2,9 @@ package beside15th11team.beside15th11team.hello;
 
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HelloRepository extends MongoRepository<Hello, Long> {
   Optional<Hello> findByBody(String body);
 }
