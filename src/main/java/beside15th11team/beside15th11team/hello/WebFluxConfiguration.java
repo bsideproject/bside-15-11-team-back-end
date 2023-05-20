c716c79abf6849f292defca025363a775a17aaac
isanghaessi <jesse1231@naver.com>
2023-05-20 20:14:43 +0900

    [fix] CORS 추가
    - 현재 도메인이 없으니, ip 추가.

--- src/main/java/beside15th11team/beside15th11team/hello/WebFluxConfiguration.java
+++ src/main/java/beside15th11team/beside15th11team/hello/WebFluxConfiguration.java
@@ -1,17 +1,22 @@
 package beside15th11team.beside15th11team.hello;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.resource.PathResourceResolver;
 
 @Configuration
-public class StaticResourceConfiguration implements WebFluxConfigurer {
+public class WebFluxConfiguration implements WebFluxConfigurer {
   private final String staticPath;
+  private final List<String> origins;
 
-  public StaticResourceConfiguration(@Value("${static.path}") String staticPath) {
+  public WebFluxConfiguration(@Value("${static.path}") String staticPath,
+                              @Value("${cors.origins}") List<String> origins) {
     this.staticPath = staticPath;
+    this.origins = origins;
   }
 
   @Override
@@ -20,5 +25,12 @@ public class StaticResourceConfiguration implements WebFluxConfigurer {
         .addResourceLocations("file:" + staticPath)
         .resourceChain(true)
         .addResolver(new PathResourceResolver());
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins(origins.toArray(String[]::new));
   }
 }
