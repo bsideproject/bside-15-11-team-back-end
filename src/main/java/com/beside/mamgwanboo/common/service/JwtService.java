8b118c84c8e8ba4061e94727aa5d0a54e40d5ee3
isanghaessi <jesse1231@naver.com>
2023-06-10 22:20:42 +0900

    [refactor] serviceImpl 삭제
    - service는 부작용 없는 로직이므로, impl이 필요 없음.
    - command만 부작용 있는 코드로 impl이 필요함.

--- src/main/java/com/beside/mamgwanboo/common/service/JwtService.java
+++ src/main/java/com/beside/mamgwanboo/common/service/JwtService.java
@@ -1,9 +1,31 @@
 package com.beside.mamgwanboo.common.service;
 
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import protobuf.sign.MamgwanbooJwtPayload;
 
-public interface JwtService {
-  String makeMamgwanbooJwt(
+@Service
+public class JwtService {
+  private static final String HEADER_ALGORITHM_KEY = "alg";
+  private static final String HEADER_ALGORITHM_HS256 = "HS256";
+  private final String baseKey;
+
+  public JwtService(@Value("${jwt.baseKey}") String baseKey) {
+    this.baseKey = baseKey;
+  }
+
+  public String makeMamgwanbooJwt(
       MamgwanbooJwtPayload mamgwanbooJwtPayload
-  );
+  ) {
+    return Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setHeaderParam(HEADER_ALGORITHM_KEY, HEADER_ALGORITHM_HS256)
+        .setPayload(mamgwanbooJwtPayload.getSequence())
+        .signWith(Keys.hmacShaKeyFor(baseKey.getBytes(StandardCharsets.UTF_8)))
+        .compact();
+  }
 }
