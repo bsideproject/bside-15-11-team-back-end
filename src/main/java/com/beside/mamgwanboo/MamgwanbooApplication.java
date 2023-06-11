0097c8392ec40a17c8d40adfcc854817b4ec9ee0
seokhan <seokhan94@naver.com>
2023-06-11 21:39:12 +0900

    [feature] mongodb reactive auditing 추가

--- src/main/java/com/beside/mamgwanboo/MamgwanbooApplication.java
+++ src/main/java/com/beside/mamgwanboo/MamgwanbooApplication.java
@@ -2,9 +2,11 @@ package com.beside.mamgwanboo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableReactiveMongoRepositories
+@EnableReactiveMongoAuditing
 @SpringBootApplication
 public class MamgwanbooApplication {
   public static void main(String[] args) {
