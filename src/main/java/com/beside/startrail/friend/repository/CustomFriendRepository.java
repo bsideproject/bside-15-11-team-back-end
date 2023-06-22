d9d8132aea4179aa02cb0f1b37f40ea55846e569
seokhan <seokhan94@naver.com>
2023-06-22 22:33:05 +0900

    [fix] friend package layer, userSequence 추가

--- src/main/java/com/beside/startrail/friend/repository/CustomFriendRepository.java
+++ src/main/java/com/beside/startrail/friend/repository/CustomFriendRepository.java
@@ -1,5 +1,6 @@
-package com.beside.startrail.friend;
+package com.beside.startrail.friend.repository;
 
+import com.beside.startrail.friend.document.Friend;
 import org.springframework.stereotype.Repository;
 import protobuf.friend.FriendSearchCriteria;
 import reactor.core.publisher.Flux;
@@ -8,5 +9,5 @@ import reactor.core.publisher.Flux;
 @Repository
 public interface CustomFriendRepository {
 
-    Flux<Friend> findFriendsByCriteria(FriendSearchCriteria keyword);
+    Flux<Friend> findFriendsByCriteria(String userSequence, FriendSearchCriteria keyword);
 }
--- src/main/java/com/beside/startrail/friend/repository/FriendRepository.java
+++ src/main/java/com/beside/startrail/friend/repository/FriendRepository.java
@@ -1,7 +1,9 @@
-package com.beside.startrail.friend;
+package com.beside.startrail.friend.repository;
 
 import com.beside.startrail.common.type.YnType;
 import java.util.UUID;
+
+import com.beside.startrail.friend.document.Friend;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,6 +11,5 @@ import reactor.core.publisher.Mono;
 
 @Repository
 public interface FriendRepository extends ReactiveMongoRepository<Friend, UUID>, CustomFriendRepository {
-    Mono<Friend> findBySequenceAndUseYn(String id, YnType useYn);
-    Flux<Friend> findByUseYn(YnType useYn);
+    Mono<Friend> findByUserSequenceAndSequenceAndUseYn(String userSequence, String sequence, YnType useYn);
 }
