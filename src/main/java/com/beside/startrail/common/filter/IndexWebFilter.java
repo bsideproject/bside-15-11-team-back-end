c138daa0a04942371495287a94a8d9204f924f1e
isanghaessi <jesse1231@naver.com>
2023-06-28 01:15:19 +0900

    [fix] redirect-uri 예외 로직 추가

--- src/main/java/com/beside/startrail/common/filter/IndexWebFilter.java
+++ src/main/java/com/beside/startrail/common/filter/IndexWebFilter.java
@@ -1,5 +1,7 @@
 package com.beside.startrail.common.filter;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -9,12 +11,18 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class IndexWebFilter implements WebFilter {
+  private final List<String> indexPaths;
+
+  public IndexWebFilter(@Value("${oauth.redirectUri}") String redirectUri) {
+    this.indexPaths = List.of("/", redirectUri);
+  }
+
   @Override
   @NonNull
   public Mono<Void> filter(ServerWebExchange exchange, @NonNull WebFilterChain chain) {
     String requestPath = exchange.getRequest().getPath().value();
 
-    if ("/".equals(requestPath)) {
+    if (indexPaths.contains(requestPath)) {
       return chain.filter(
           exchange.mutate()
               .request(
