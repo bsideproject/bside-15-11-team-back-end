efc878b40ecb44e73a08a6946914771065a8d2ab
isanghaessi <jesse1231@naver.com>
2023-06-28 00:03:48 +0900

    [test] https test

--- src/main/java/com/beside/startrail/common/configuration/HttpRedirectConfiguration.java
+++ src/main/java/com/beside/startrail/common/configuration/HttpRedirectConfiguration.java
@@ -1,12 +1,13 @@
 package com.beside.startrail.common.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Profile("release")
-//@Configuration
+@Configuration
 public class HttpRedirectConfiguration {
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
