dee690396fd22cd39385ebf58e58f48daf01d08b
isanghaessi <jesse1231@naver.com>
2023-07-16 00:20:38 +0900

    [fix] TC 수정

--- src/test/java/com/beside/startrail/StartrailApplicationTest.java
+++ src/test/java/com/beside/startrail/StartrailApplicationTest.java
@@ -2,8 +2,10 @@ package com.beside.startrail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class StartrailApplicationTest {
   @Test
   void contextLoads() {
